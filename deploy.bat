c0aaf7d033e29aa6343e676c635308d62bf43a8f
Debutant610 <mrkaldine@gmail.com>
2026-04-03 01:05:52 +0400

    feat: lancement production avec base de donnees reelle et api binance

--- deploy.bat
+++ deploy.bat
@@ -11,7 +11,7 @@ echo 2. Sauvegarde des nouvelles fonctionnalites...
 git commit -m "feat: lancement production avec base de donnees reelle et api binance"
 echo.
 echo 3. Envoi vers GitHub et Vercel...
-git push origin main
+git push origin main -f
 echo.
 echo ========================================================
 echo SUCCES ! Vercel est en train de compiler le site.
